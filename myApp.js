7378273b8877597ec1473e9d364d7476726d3795
Harrison King <authorharrisonking@gmail.com>
2021-01-23 17:21:04 -0700

    use the .env file

--- myApp.js
+++ myApp.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var app = express();
- app.get('/',function(req,res) {
-    res.sendFile(__dirname + '/views/index.html')
- });
+    process.env.MESSAGE_STYLE=uppercase;
 
- app.use('/',express.static(__dirname + '/public'));
+    app.get('/',function(req,res) {
+        res.sendFile(__dirname + '/views/index.html')
+    });
 
- app.get('/json', function(req, res) {
-    res.json({"message": "Hello json"})
- });
+    app.use('/',express.static(__dirname + '/public'));
+
+    app.get('/json', function(req, res) {
+        if (process.env.MESSAGE_STYLE === 'uppercase') {
+            res.json({"message": "Hello json"}).toUpperCase();
+        }
+        
+    });
+
+    
 
 
 
