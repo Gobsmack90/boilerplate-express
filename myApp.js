83a891e72ad32ba65ed293b81081c8aee669caad
Harrison King <authorharrisonking@gmail.com>
2021-01-23 17:07:56 -0700

    serve JSON on a specific route

--- myApp.js
+++ myApp.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
  app.get('/',function(req,res) {
-     res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html')
  });
 
  app.use('/',express.static(__dirname + '/public'));
 
-
+ app.get('/json', function(req, res) {
+    res.json({"message": "Hello json"})
+ });
 
 
 
