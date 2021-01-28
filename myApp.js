66fc8644dcc8bbd2e4aa7101adabf32fe90b8dd1
Harrison King <authorharrisonking@gmail.com>
2021-01-27 17:45:56 -0700

    implement root-level request logger middleware

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
     process.env.MESSAGE_STYLE="uppercase";
 
+    //middleware to log method, route path and user id when request is made.
+    app.use((req, res, next) => {
+        console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+        next();
+    })
+
     app.get('/',function(req,res) {
         res.sendFile(__dirname + '/views/index.html')
     });
